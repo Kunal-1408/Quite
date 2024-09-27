10b80d124b5bd5aaf54da565e5918190263496e8
Kunal <srivastavakunal14@gmail.com>
2024-09-27 17:29:45 +0530

    finalised cms

--- app/(CMS)/Dashboard/page.tsx
+++ app/(CMS)/Dashboard/page.tsx
@@ -45,15 +45,15 @@ export default function Dashboard() {
 
   if (!isMounted) return null; 
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr] bg-neutral-100 dark:bg-neutral-700">
       <div className="hidden border-r bg-muted/40 md:block">
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-            <Link href="/" className="flex items-center gap-2 font-semibold">
+            <Link href="/" className="flex items-center gap-2 font-semibold text-neutral-500">
               <Package2 className="h-6 w-6" />
               <span className="">Acme Inc</span>
             </Link>
-            <button className="ml-auto h-8 w-8 border border-input bg-background hover:bg-accent hover:text-accent-foreground">
+            <button className=" ml-auto h-8 w-8 border border-input bg-inherit hover:bg-accent hover:text-accent-foreground">
               <Bell className="h-4 w-4" />
               <span className="sr-only">Toggle notifications</span>
             </button>
@@ -100,22 +100,7 @@ export default function Dashboard() {
               </Link>
             </nav>
           </div>
-          <div className="mt-auto p-4">
-            <Card x-chunk="dashboard-02-chunk-0">
-              <CardHeader className="p-2 pt-0 md:p-4">
-                <CardTitle>Upgrade to Pro</CardTitle>
-                <CardDescription>
-                  Unlock all features and get unlimited access to our support
-                  team.
-                </CardDescription>
-              </CardHeader>
-              <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
-                <button className="w-full h-9 rounded-md px-3">
-                  Upgrade
-                </button>
-              </CardContent>
-            </Card>
-          </div>
+
         </div>
       </div>
       <div className="flex flex-col">
@@ -177,22 +162,7 @@ export default function Dashboard() {
                   Analytics
                 </Link>
               </nav>
-              <div className="mt-auto">
-                <Card>
-                  <CardHeader>
-                    <CardTitle>Upgrade to Pro</CardTitle>
-                    <CardDescription>
-                      Unlock all features and get unlimited access to our
-                      support team.
-                    </CardDescription>
-                  </CardHeader>
-                  <CardContent>
-                    <button className="w-full h-9 rounded-md px-3">
-                      Upgrade
-                    </button>
-                  </CardContent>
-                </Card>
-              </div>
+
             </SheetContent>
           </Sheet>
           <div className="w-full flex-1">
