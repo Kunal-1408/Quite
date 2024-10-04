be7bf59e5bafd4a050b081e6329cce22cecbb748
Kunal <srivastavakunal14@gmail.com>
2024-10-04 16:43:01 +0530

    ...

--- app/CMS/website_manager/page.tsx
+++ app/CMS/website_manager/page.tsx
@@ -1,11 +1,173 @@
-import Link from "next/link";
-import Image from "next/image";
-import React from "react";
+import Image from "next/image"
 
-export default function manager(){
-    return(
-        <div>
-            <span>welcome to manager</span>
-        </div>
-    )
+import {
+  File,
+  ListFilter,
+  MoreHorizontal,
+  PlusCircle,
+
+} from "lucide-react"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { prisma } from "@/lib/prisma"
+
+
+export default async function Manager() {
+    const projects = await prisma.Web
+    console.log(projects)
+  return (
+    <div className="flex min-h-screen w-full flex-col bg-muted/40">
+        <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+
+            <div className="flex flex-col">
+              <div className="ml-auto flex items-center gap-2">
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <button  className="h-8 rounded-md px-3 gap-1 border border-input bg-background hover:bg-accent hover:text-accent-foreground bg-slate-100 hover:bg-slate-200 flex flex-row">
+                      <ListFilter className="h-3.5 w-3.5" />
+                      <span className="sr-only sm:not-sr-only sm:whitespace-nowrap" >
+                        Filter
+                      </span>
+                    </button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>Filter by</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuCheckboxItem checked>
+                      Active
+                    </DropdownMenuCheckboxItem>
+                    <DropdownMenuCheckboxItem>Draft</DropdownMenuCheckboxItem>
+                    <DropdownMenuCheckboxItem>
+                      Archived
+                    </DropdownMenuCheckboxItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+                <button  className="h-8 rounded-md px-3 gap-1 border border-input bg-background hover:bg-accent hover:text-accent-foreground bg-slate-100 hover:bg-slate-200 flex flex-row">
+                  <File className="h-3.5 w-3.5" />
+                  <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                    Export
+                  </span>
+                </button>
+                <button className="h-8 gap-1 rounded-md px-3 bg-slate-100 hover:bg-slate-200 flex flex-row">
+                  <PlusCircle className="h-5 w-5 items-center"  />
+                  <span className="sr-only sm:not-sr-only sm:whitespace-nowrap ">
+                    Add Website
+                  </span>
+                </button>
+              </div>
+            </div>
+            
+              <Card x-chunk="dashboard-06-chunk-0">
+                <CardHeader>
+                  <CardTitle>Websites</CardTitle>
+                  <CardDescription>
+                    Manage your Websites and their performance
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    {projects.map(())}
+                  <Table>
+                    <TableHeader>
+                      <TableRow>
+                        <TableHead className="hidden w-[100px] sm:table-cell">
+                          <span className="sr-only">Image</span>
+                        </TableHead>
+                        <TableHead>Name</TableHead>
+                        <TableHead>Status</TableHead>
+                        <TableHead className="hidden md:table-cell">
+                          Price
+                        </TableHead>
+                        <TableHead className="hidden md:table-cell">
+                          Total Sales
+                        </TableHead>
+                        <TableHead className="hidden md:table-cell">
+                          Created at
+                        </TableHead>
+                        <TableHead>
+                          <span className="sr-only">Actions</span>
+                        </TableHead>
+                      </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                      <TableRow>
+                        <TableCell className="hidden sm:table-cell">
+                          <Image
+                            alt="Product image"
+                            className="aspect-square rounded-md object-cover"
+                            height="64"
+                            src="/placeholder.svg"
+                            width="64"
+                          />
+                        </TableCell>
+                        <TableCell className="font-medium">
+                          
+                        </TableCell>
+                        <TableCell>
+                          <span className="inline-flex text-foreground items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2" >Draft</span>
+                        </TableCell>
+                        <TableCell className="hidden md:table-cell">
+                          $499.99
+                        </TableCell>
+                        <TableCell className="hidden md:table-cell">
+                          25
+                        </TableCell>
+                        <TableCell className="hidden md:table-cell">
+                          2023-07-12 10:42 AM
+                        </TableCell>
+                        <TableCell>
+                          <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                              <button
+                                aria-haspopup="true" className="hover:bg-accent hover:text-accent-foreground h-10 w-10"
+                              >
+                                <MoreHorizontal className="h-4 w-4" />
+                                <span className="sr-only">Toggle menu</span>
+                              </button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent align="end">
+                              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                              <DropdownMenuItem>Edit</DropdownMenuItem>
+                              <DropdownMenuItem>Delete</DropdownMenuItem>
+                            </DropdownMenuContent>
+                          </DropdownMenu>
+                        </TableCell>
+                      </TableRow>
+                    </TableBody>
+                  </Table>
+                </CardContent>
+                <CardFooter>
+                  <div className="text-xs text-muted-foreground">
+                    Showing <strong>1-10</strong> of <strong>32</strong>{" "}
+                    products
+                  </div>
+                </CardFooter>
+              </Card>
+        </main>
+      </div>
+  )
 }
